d9beba751d3b5a688a5531c2ef69fd3c6f4a8c2d
Manideep317 <manideepgummadi05@gmail.com>
2024-05-24 10:30:33 +0530

    add login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,13 +1,66 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
-export default function page() {
+export default function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === 'admin@gmail.com') {
+      router.push('/admin/Companies');
+    } else {
+      alert('Invalid email');
+    }
+  };
+
   return (
-    <main className='w-full bg-white h-screen'>
-      <div className="">
-        <form action="">
-            
-        </form>
+    <section className="bg-white">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl">
+              Login to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-black">Your email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-black sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+              </div>
+              <button type="submit" className="w-full text-white bg-primary-600 bg-black hover:bg-white hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+    </section>
+  );
 }
